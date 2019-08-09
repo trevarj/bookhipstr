47af327ccad258a96cfeb3581d3e20cd20d7247a
William Whatley <william@catatek.com>
2019-08-09 11:00:41 -0500

    Added video pg.

--- src/packages/videography-services.md
+++ src/packages/videography-services.md
@@ -3,13 +3,13 @@ path: /videography
 date: "2019-03-11"
 title: "Videography"
 image: "https://i.postimg.cc/NjdnHZnB/flow-Cam-Home.png"
-splash: "../images/photography-splash.jpg"
+splash: "../images/video-splash.jpg"
 subtitle: "It’s important to document the moments that matter and sometimes still images aren’t enough. Our Videography packages are tailored to your video needs. Our Corporate Videography packages are built around your professional needs, while our Social videography packages were designed to capture the essence of a couple’s story or any of your special days."
-tagline: "Hipstr’s Flow Cam is the most seamless photo activation ever…"
+tagline: "Hipstr’s video services are exactly what you need to showcase your brand or event in the way that makes you shine."
 excerpt1: "A unique experience with bullet-time GIFs & more."
 excerpt2: "SURE TO IMPRESS."
 type: "image"
 key: "services"
 ---
 
-<img alt="Hipstr Flow Cam" src="../images/photography-grid.jpg">
+<iframe src="https://player.vimeo.com/video/352340154" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
--- src/templates/package.js
+++ src/templates/package.js
@@ -7,12 +7,11 @@ import OneSplash from "../components/onesplash";
 import PackageBanner from "../components/packagebanner";
 import Testimonials from "../components/testimonials";
 import SEO from "../components/seo";
-import { Text, Subtitle, Column } from "../theme/index";
 
 const Grid = styled.div`
   display: grid;
   grid-gap: 50px;
-  width: 80%;
+  width: 85%;
   margin: 1em auto 0 auto;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-auto-rows: auto;
@@ -27,6 +26,7 @@ const Information = styled.div`
   width: 100%;
   height: 100%;
   margin: auto;
+  margin-top: 1em;
 `;
 
 const Div = styled.div`
@@ -35,41 +35,9 @@ const Div = styled.div`
   margin-bottom: 2em;
 `;
 
-const Li = styled.li`
-  font-family: "Poppins Regular", sans-serif;
-  color: #375279;
-  font-weight: 400;
-  font-size: 12px;
-`;
-
-const StyledColumn = styled(Column)`
-  width: 95%;
-  margin: 0 auto;
-`;
-
-function Form({ type }) {
+function Form() {
   return (
     <Div>
-      {type === "/photography" && (
-        <StyledColumn>
-          <Subtitle large>
-            {<span style={{ color: "#ec7673" }}>Hipstr</span>} Photography
-          </Subtitle>
-          <Text>
-            Hipstr’s all-inclusive Photography Packages were carefully crafted
-            with our Social and Corporate clients in mind. The packages capture
-            all the essential elements of your event professionally and
-            creatively and are tailored to your specific needs.
-          </Text>
-          <ul style={{ marginTop: "1em" }}>
-            <Li>A Personable & Professional Photographer</Li>
-            <Li>8 Hours of Continuous Coverage</Li>
-            <Li>500 Edited High Resolution Images (with print rights)</Li>
-            <Li>Online Gallery with Processed Photos for Viewing</Li>
-            <Li>Digital Download Delivery</Li>
-          </ul>
-        </StyledColumn>
-      )}
       <iframe
         id="JotFormIFrame-80186446884165"
         onLoad={typeof window !== "undefined" && window.parent.scrollTo(0, 0)}
@@ -115,11 +83,10 @@ export default function Package({ data }) {
           className="package"
           dangerouslySetInnerHTML={{ __html: html }}
         />
+
         <Form type={frontmatter.path} />
       </Grid>
-      {frontmatter.path !== "/photography" && (
-        <PackageBanner title={frontmatter.tagline} />
-      )}
+      <PackageBanner title={frontmatter.tagline} />
       <Testimonials />
     </Layout>
   );
