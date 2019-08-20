3d3fdff4d2ddd9d35d3ca78c52eaeced1b0a3d5b
William Whatley <william@catatek.com>
2019-08-20 09:45:17 -0500

    ...

--- src/pages/404.js
+++ src/pages/404.js
@@ -20,26 +20,7 @@ const NotFoundPage = () => (
     <SEO title="404 | Not found" />
     <div style={{ height: "75px" }} />
     <Wrapper>
-      <Subtitle>404 | Not found</Subtitle>
-      <Text bold>Maybe you were looking for these links instead:</Text>
-      <Link to="/">
-        <Text>Home</Text>
-      </Link>
-      <a href="https://competent-brattain-44ae2d.netlify.com/static/Event-Attendant-Posting-Final-CHICAGO.pdf">
-        <Text>Event attendant posting | Chicago</Text>
-      </a>
-      <a href="https://competent-brattain-44ae2d.netlify.com/static/Event-Attendant-Posting-Final-LA.pdf">
-        <Text>Event attendant posting | LA</Text>
-      </a>
-      <a href="https://competent-brattain-44ae2d.netlify.com/static/Event-Attendant-Posting-Final-NY.pdf">
-        <Text>Event attendant posting | NY</Text>
-      </a>
-      <a href="https://competent-brattain-44ae2d.netlify.com/static/Event-Attendant-Posting-Final-PA.pdf">
-        <Text>Event attendant posting | PA</Text>
-      </a>
-      <a href="https://competent-brattain-44ae2d.netlify.com/static/Event-Attendant-Posting-Final-SD.pdf">
-        <Text>Event attendant posting | SD</Text>
-      </a>
+      <h1>Page not found!</h1>
     </Wrapper>
   </Layout>
 );
